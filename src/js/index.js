fca97002e4ace1afa204116aa04b3d55f3a7a5eb
Haruki Jay Shimada <peaske16180@gmail.com>
2020-04-23 01:51:08 +0900

    added deadline_section and steps_section to html

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,53 @@
-$(document).ready(() => {
-    // $('.js--section-features').waypoint(function(direction) {
-    //     if (direction == "down") {
-    //         $('nav').addClass('sticky');
-    //     } else {
-    //         $('nav').removeClass('sticky');   
-    //     }
-    // }, {
-    //     offset: '60px;'
-    // });
-    
-    // scroll sections
-    $('.js--scroll-to-reasons').click(function() {
-        $('html, body').animate({scrollTop: $('.js--section-reasons').offset().top}, 1000)
+// $(document).ready(() => {
+//     $('.js--scroll-to-reasons').click(function() {
+//         $('html, body').animate({scrollTop: $('.js--section-banner').offset().top}, 1000)
+//     });
+// });
+
+;(function(document, window, index) {
+    $(document).ready(() => {
+        $('.js--scroll-to-reasons').click(function() {
+            $('html, body').animate({scrollTop: $('.js--section-banner').offset().top}, 1000)
+        });
     });
-});
+
+    // 'use strict';
+
+    var element = document.querySelector('.header_nav');
+
+    if (!element) return true;
+
+    var elHeight = 0,
+        elTop = 0,
+        dHeight = 0,
+        wHeight = 0,
+        wScrollCurrent = 0,
+        wScrollBefore = 0,
+        wScrollDiff = 0;
+
+    window.addEventListener('scroll', function() {
+        elHeight = element.offsetHeight;
+        dHeight = document.body.offsetHeight;
+        wHeight = window.innerHeight;
+        wScrollCurrent = window.pageYOffset;
+        wScrollDiff = wScrollBefore - wScrollCurrent;
+        elTop = parseInt(window.getComputedStyle(element).getPropertyValue('top')) + wScrollDiff;
+
+        if (wScrollCurrent <= 0)
+            element.style.top = '0px';
+
+        else if (wScrollDiff > 0)
+            element.style.top = (elTop > 0 ? 0 : elTop) + 'px';
+
+        else if (wScrollDiff < 0) {
+            if (wScrollCurrent + wHeight >= dHeight - elHeight)
+                element.style.top = ((elTop = wScrollCurrent + wHeight - dHeight) < 0 ? elTop : 0) + 'px';
+
+            else
+                element.style.top = (Math.abs(elTop) > elHeight ? -elHeight : elTop) + 'px';
+        }
+
+        wScrollBefore = wScrollCurrent;
+    });
+
+}(document, window, 0));
