360adba6053f03a38be1656f3b3f524eac1d5c2c
Haruki Jay Shimada <peaske16180@gmail.com>
2020-04-25 18:28:31 +0900

    re-did the content_section and added a fixed header

--- src/js/index.js
+++ src/js/index.js
@@ -1,53 +1,35 @@
-// $(document).ready(() => {
-//     $('.js--scroll-to-reasons').click(function() {
-//         $('html, body').animate({scrollTop: $('.js--section-banner').offset().top}, 1000)
-//     });
-// });
-
-;(function(document, window, index) {
-    $(document).ready(() => {
-        $('.js--scroll-to-reasons').click(function() {
-            $('html, body').animate({scrollTop: $('.js--section-banner').offset().top}, 1000)
-        });
+$(document).ready(() => {
+    $('.js--scroll-to-reasons').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-banner').offset().top}, 500)
     });
+});
 
-    // 'use strict';
+$(document).ready(() => {
+    $('.header_nav__item--top').click(() => {
+        $('html, body').animate({scrollTop: $('.page__header').offset().top}, 500)
+    });
+});
 
-    var element = document.querySelector('.header_nav');
+$(document).ready(() => {
+    var previousScroll = 0,
+        headerOrgOffset = $('.header_nav').offset().top;
 
-    if (!element) return true;
-
-    var elHeight = 0,
-        elTop = 0,
-        dHeight = 0,
-        wHeight = 0,
-        wScrollCurrent = 0,
-        wScrollBefore = 0,
-        wScrollDiff = 0;
-
-    window.addEventListener('scroll', function() {
-        elHeight = element.offsetHeight;
-        dHeight = document.body.offsetHeight;
-        wHeight = window.innerHeight;
-        wScrollCurrent = window.pageYOffset;
-        wScrollDiff = wScrollBefore - wScrollCurrent;
-        elTop = parseInt(window.getComputedStyle(element).getPropertyValue('top')) + wScrollDiff;
-
-        if (wScrollCurrent <= 0)
-            element.style.top = '0px';
-
-        else if (wScrollDiff > 0)
-            element.style.top = (elTop > 0 ? 0 : elTop) + 'px';
-
-        else if (wScrollDiff < 0) {
-            if (wScrollCurrent + wHeight >= dHeight - elHeight)
-                element.style.top = ((elTop = wScrollCurrent + wHeight - dHeight) < 0 ? elTop : 0) + 'px';
-
-            else
-                element.style.top = (Math.abs(elTop) > elHeight ? -elHeight : elTop) + 'px';
+    $(window).scroll(function() {
+        var currentScroll = $(this).scrollTop();
+        if(currentScroll > headerOrgOffset) {
+            if (currentScroll > previousScroll) {
+                $('.header_nav').fadeOut(200);
+            } else {
+                $('.header_nav').fadeIn(200);
+                $('.header_nav').addClass('fixed');
+            }
+        } else {
+             $('.header_nav').removeClass('fixed');   
         }
-
-        wScrollBefore = wScrollCurrent;
+        previousScroll = currentScroll;
     });
+});
 
-}(document, window, 0));
+$(document).ready(() => {
+
+});
