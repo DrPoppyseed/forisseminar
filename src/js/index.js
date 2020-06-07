6dd868ec09d8a128b0d493506a7a55ea6ab4adb5
Haruki Jay Shimada <peaske16180@gmail.com>
2020-06-07 14:21:23 +0900

    small changes

--- src/js/index.js
+++ src/js/index.js
@@ -205,20 +205,20 @@ $(document).ready(function() {
             $('.steps__step_detail--title').html(`<h3>お支払い</h3>`);
             $('.steps__step_detail--text').html(`<p>選択したプランの料金を、銀行振込にて、お支払いください。銀行振り込みの手順に関しましては、こちらから送付させていただきます。</p>`);
         } else if (slideIndex == 2) {
-            $('.steps__step_detail--title').html(`<h3>セミナーで使用するテキストの購入（プレミアムプランご購入の方はテキストが付属です）</h3>`);
-            $('.steps__step_detail--text').html(`<p>FORIS専用のNoteアカウントにアクセスしていただいたのち、セミナーに必要なテキストを購入していただきます。ただし、プレミアムプランをご購入の方はプラン特典として、テキストが無料で送付させていただきます。</p>`);
+            $('.steps__step_detail--title').html(`<h3>授業で使用するテキストの購入（プレミアムプランご購入の方はテキストが付属です）</h3>`);
+            $('.steps__step_detail--text').html(`<p>FORIS専用のNoteアカウントにアクセスしていただいたのち、受講に必要なテキストを購入していただきます。ただし、プレミアムプランをご購入の方はプラン特典として、テキストが無料で送付させていただきます。</p>`);
         } else if (slideIndex == 3) {
-            $('.steps__step_detail--title').html(`<h3>お客様とのセミナー日程調整</h3>`);
+            $('.steps__step_detail--title').html(`<h3>お客様との受講日程調整</h3>`);
             $('.steps__step_detail--text').html(`<p>講師とのマンツーマン授業となりますので、お客様がお選びになった学期内で、お客様と講師の都合が着く日程を調整していただきます。</p>`);
         } else if (slideIndex == 4) {
             $('.steps__step_detail--title').html(`<h3>FORISよりZoomのリンクを送付</h3>`);
             $('.steps__step_detail--text').html(`<p>授業開始の数日前にはこちらより、ZOOMのリンクをお客様に送らせていただきます。</p>`);
         } else if (slideIndex == 5) {
-            $('.steps__step_detail--title').html(`<h3>セミナー開始</h3>`);
+            $('.steps__step_detail--title').html(`<h3>受講開始</h3>`);
             $('.steps__step_detail--text').html(`<p>お客様のお選びになった日程に沿って、カリキュラムを進めていきます。</p>`);
         } else if (slideIndex == 6) {
-            $('.steps__step_detail--title').html(`<h3>セミナー全て終了後、質疑応答ができるオンラインサロンへのご招待</h3>`);
-            $('.steps__step_detail--text').html(`<p>セミナー終了後、セミナー後に上がった質疑の応答や、同じセミナーを受けた仲間と混じり合えるオンラインサロンへご招待させていただきます。</p>`);
+            $('.steps__step_detail--title').html(`<h3>全て終了後、質疑応答ができるオンラインサロンへのご招待</h3>`);
+            $('.steps__step_detail--text').html(`<p>受講終了後、質疑の応答や、同じ授業を受けた仲間と混じり合えるオンラインサロンへご招待させていただきます。</p>`);
         }
     }
 
